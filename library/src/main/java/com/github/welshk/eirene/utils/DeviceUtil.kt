0f56a427cf147574a9814b08ba0b57de236f08ad
Kevin <welshk91@gmail.com>
2019-04-20 05:43:39 -0400

    Fix possible issue with hiding system UI if on devices older than KitKat

--- library/src/main/java/com/github/welshk/eirene/utils/DeviceUtil.kt
+++ library/src/main/java/com/github/welshk/eirene/utils/DeviceUtil.kt
@@ -1,6 +1,7 @@
 package com.github.welshk.eirene.utils
 
 import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.view.View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
 import android.view.View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
@@ -16,11 +17,18 @@ class DeviceUtil {
         @JvmStatic
         @Throws(NullPointerException::class)
         fun hideSystemUi(context: Context) {
-            mSystemUiVisibility = (SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    or SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    or SYSTEM_UI_FLAG_FULLSCREEN
-                    or SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                    or SYSTEM_UI_FLAG_IMMERSIVE)
+            mSystemUiVisibility = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                (SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        or SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        or SYSTEM_UI_FLAG_FULLSCREEN
+                        or SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        or SYSTEM_UI_FLAG_IMMERSIVE)
+            } else {
+                (SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        or SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        or SYSTEM_UI_FLAG_FULLSCREEN
+                        or SYSTEM_UI_FLAG_HIDE_NAVIGATION)
+            }
 
             (context as AppCompatActivity).supportActionBar!!.hide()
             context.window.decorView.systemUiVisibility = mSystemUiVisibility
