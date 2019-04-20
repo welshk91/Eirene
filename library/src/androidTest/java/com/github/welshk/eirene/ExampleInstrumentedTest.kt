16bb13db689ddda6caf48d90ae62719066cd5185
Kevin <welshk91@gmail.com>
2019-04-20 05:51:38 -0400

    Moved tests file from java to kotlin

--- library/src/androidTest/java/com/github/welshk/eirene/ExampleInstrumentedTest.kt
+++ library/src/androidTest/java/com/github/welshk/eirene/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.github.welshk.eirene.demo
+package com.github.welshk.eirene
 
 import androidx.test.InstrumentationRegistry
 import androidx.test.runner.AndroidJUnit4
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith
 /**
  * Instrumented test, which will execute on an Android device.
  *
- * See [testing documentation](http://d.android.com/tools/testing).
+ * @see [Testing documentation](http://d.android.com/tools/testing)
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.github.welshk.eirene.demo", appContext.packageName)
+        assertEquals("com.github.welshk.eirene.test", appContext.packageName)
     }
 }
